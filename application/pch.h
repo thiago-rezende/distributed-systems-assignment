bed4737670165a08b58a99e1f67d59ba2fb4edd0
Thiago Rezende <bitrateti@gmail.com>
2021-01-07 18:34:41 -0300

    Initial commit

---
+++ application/pch.h
@@ -0,0 +1,15 @@
+/**
+ * @file pch.h
+ * @author Thiago Rezende (thiago-rezende.github.io)
+ * @brief Pre-Compiled Header
+ * @version 0.1
+ * @date 2020-09-26
+ *
+ * @copyright Copyright (c) 2020 Thiago Rezende
+ *
+ */
+
+#pragma once
+
+#include <iostream>
+#include <application/core/logger.h>
