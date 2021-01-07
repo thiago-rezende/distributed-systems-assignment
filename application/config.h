54d706f72ecd4ebd1856568a7b5e3abd73086e5b
Thiago Rezende <bitrateti@gmail.com>
2021-01-07 20:16:44 -0300

    docs: changed project name and description

--- application/config.h
+++ application/config.h
@@ -1,15 +1,15 @@
 #pragma once
 
 /* Porject Name */
-#define PROJECT_NAME "CMakeStarterProject"
+#define PROJECT_NAME "DistributedSystemsAssignment"
 
-#define PROJECT_DESCRIPTION "A Simple C++ Starter Project Using CMake"
-#define PROJECT_HOMEPAGE_URL "https://github.com/thiago-rezende/cmake-starter-project"
+#define PROJECT_DESCRIPTION "Distributed Systems Assignment"
+#define PROJECT_HOMEPAGE_URL "https://github.com/thiago-rezende/distributed-systems-assignment"
 
 /* Project Version [SEMVER] */
-#define PROJECT_VERSION_MAJOR 2
-#define PROJECT_VERSION_MINOR 2
+#define PROJECT_VERSION_MAJOR 0
+#define PROJECT_VERSION_MINOR 0
 #define PROJECT_VERSION_PATCH 1
 
 /* Porject Version String [SEMVER] */
-#define PROJECT_VERSION "v2.2.1"
+#define PROJECT_VERSION "v0.0.1"
